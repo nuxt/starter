a07b9e12ff8cbe46d697a9afc962f2d1b0669350
Pooya Parsa <pyapar@gmail.com>
2021-10-12 15:53:16 +0200

    update v3

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Hello Nuxt 3!</h1>
-    <a href="https://v3.nuxtjs.org" target="_blank" rel="noopener">Read the docs</a>
+    <!-- Remove this component to get started! -->
+    <NuxtWelcome />
   </div>
 </template>
