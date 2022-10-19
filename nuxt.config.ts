243d72093f05f1efa35f0851d37bf2ec35eec090
Pooya Parsa <pooya@pi0.io>
2022-10-19 12:13:45 +0200

    disable `writeEarlyHints` as temporary workaround

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  experimental: {
+    // Temporary workaround for stackblitz
+    writeEarlyHints: false,
+  }
 })
