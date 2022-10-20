e8dcc89157fb5c407d776205442b75a1747a718a
Daniel Roe <daniel@roe.dev>
2022-10-20 14:35:48 +0100

    chore: re-enable `writeEarlyHints`
    This reverts commit 243d72093f05f1efa35f0851d37bf2ec35eec090.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  experimental: {
-    // Temporary workaround for stackblitz
-    writeEarlyHints: false,
-  }
+
 })
