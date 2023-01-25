50cd9cac851460cd39f052aefc43f43f903747b4
lihbr <lihbr@users.noreply.github.com>
2023-01-25 18:16:18 +0100

    chore: cleanup default module

--- src/module.ts
+++ src/module.ts
@@ -1,24 +1,21 @@
-import { fileURLToPath } from 'url'
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
-export interface ModuleOptions {
-  addPlugin: boolean
-}
+// Module options TypeScript inteface definition
+export interface ModuleOptions {}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
     name: 'my-module',
     configKey: 'myModule'
   },
-  defaults: {
-    addPlugin: true
-  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
   setup (options, nuxt) {
-    if (options.addPlugin) {
-      const { resolve } = createResolver(import.meta.url)
-      const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
-      nuxt.options.build.transpile.push(runtimeDir)
-      addPlugin(resolve(runtimeDir, 'plugin'))
-    }
+    const { resolve } = createResolver(import.meta.url)
+
+    const runtimeDir = resolve('./runtime')
+    nuxt.options.build.transpile.push(runtimeDir)
+
+    addPlugin(resolve(runtimeDir, 'plugin'))
   }
 })
