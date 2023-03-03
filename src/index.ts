358482cb7ae624c3c8a0d5082bda2d9dde94826b
Sébastien Chopin <seb@nuxt.com>
2023-03-03 11:22:23 +0100

    chore: add index.ts in module starter

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * This file won't be run by Nuxt when loading the module.
+ *
+ * It is used to expose utils for the user.
+ *
+ * export const myUtil = () => console.log('This is myUtil from the module')
+ */
+
+/**
+ * The module user can:
+ *
+ * import { myUtil } from 'my-module'
+ */
+
+/**
+ * You can safely remove this file if you don't need to expose utils.
+ */
