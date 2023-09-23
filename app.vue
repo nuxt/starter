3d63146f84848ebb53babd3388742f958edc4e8f
Sébastien Chopin <seb@nuxt.com>
2023-09-23 16:20:06 +0200

    chore: add ui starter

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UContainer>
+    <UCard class="mt-10">
+      <template #header>
+        <div class="flex justify-between">
+          <h1>Welcome to Nuxt UI Starter</h1>
+          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+        </div>
+      </template>
+      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+    </UCard>
+  </UContainer>
 </template>
