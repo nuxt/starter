215dcd75b6bb78cb8e1695d9bb2103c0e40965a3
Daniel Roe <daniel@roe.dev>
2023-12-25 17:52:04 +0000

    chore: update import subpath

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
-import { setup, $fetch } from '@nuxt/test-utils'
+import { setup, $fetch } from '@nuxt/test-utils/e2e'
 
 describe('ssr', async () => {
   await setup({
