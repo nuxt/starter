b558638c53cb25ef078a35c0a40f6a2cffa6eec4
Anthony Fu <anthonyfu117@hotmail.com>
2024-04-09 14:25:40 +0200

    feat(module): use ESLint v9 and Flat Config

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+// Run `npx @eslint/config-inspector` to inspect the resolved config interactively
+export default createConfigForNuxt({
+  features: {
+    // Rules for Module Authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: [
+      './playground',
+    ],
+  },
+})
+  .append(
+    // you custom flat config here...
+  )
