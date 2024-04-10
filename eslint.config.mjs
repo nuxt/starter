3859dcaf32fc2150cf0d3c66ecde693857b32b72
Daniel Roe <daniel@roe.dev>
2024-04-10 10:42:10 +0200

    chore: update comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
   features: {
-    // Rules for Module Authors
+    // Rules for module authors
     tooling: true,
     // Rules for formatting
     stylistic: true,
@@ -16,5 +16,5 @@ export default createConfigForNuxt({
   },
 })
   .append(
-    // you custom flat config here...
+    // your custom flat config here...
   )
