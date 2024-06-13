4567b80a2dbc73cfde874ae339dbd57699a6b5ba
Farnabaz <farnabaz@gmail.com>
2024-06-13 11:10:39 +0200

    fix(generate): workaround for routes pre-render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  routeRules: {
+    '/': { prerender: true }
+  }
 })
