9e25b2606c5d431d7116fa9797b13a836a2558f5
Anthony Fu <github@antfu.me>
2024-08-21 14:54:56 +0200

    chore: update client nuxt.config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,15 +2,24 @@ import { resolve } from 'pathe'
 
 export default defineNuxtConfig({
   ssr: false,
+
   modules: [
     '@nuxt/devtools-ui-kit',
   ],
+
   nitro: {
     output: {
       publicDir: resolve(__dirname, '../dist/client'),
     },
   },
+
   app: {
     baseURL: '/__my-module',
   },
+
+  devtools: {
+    enabled: false,
+  },
+
+  compatibilityDate: '2024-08-21',
 })
