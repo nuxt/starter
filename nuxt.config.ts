c199ad4645701dfba901cf66ced8eb0766de7a57
Daniel Roe <daniel@roe.dev>
2025-07-16 10:04:31 +0200

    chore: update compat date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
